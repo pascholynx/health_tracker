23add3c7fc3d3e788eba15dc524dcd5ab6df758d
pascholynx <dozieinya2002@gmail.com>
2023-12-19 02:30:03 +0100

    Updated Dashboard UI

--- app/src/main/java/com/example/healthtrackpro/DashboardActivity.java
+++ app/src/main/java/com/example/healthtrackpro/DashboardActivity.java
@@ -1,15 +1,81 @@
 package com.example.healthtrackpro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private TextView username;
+
+    // Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Initialize Firebase Database reference
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
+
+        // Initialize the TextView
+        username = findViewById(R.id.username);
+
+        ImageView logoutImageView = findViewById(R.id.imageView3);
+
+        logoutImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the main activity
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        if (currentUser != null) {
+            // Get the current user's UID
+            String userId = currentUser.getUid();
+
+            // Retrieve the username from Firebase Database using the UID
+            usersRef.child(userId);
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    // Retrieve the username and set it in the TextView
+                    if (dataSnapshot.exists()) {
+                        User user = dataSnapshot.getValue(User.class);
+                        String userName = user.getUsername();
+                        username.setText(userName);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle any errors
+                }
+            });
+        }
     }
 }
