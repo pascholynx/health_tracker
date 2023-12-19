9f069aa23dd0868110d534fc4fd009b239eb8089
ekegrant59 <107748298+ekegrant59@users.noreply.github.com>
2023-12-19 08:59:17 +0100

    changes

--- app/src/main/java/com/example/healthtrackpro/DashboardActivity.java
+++ app/src/main/java/com/example/healthtrackpro/DashboardActivity.java
@@ -17,7 +17,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class DashboardActivity extends AppCompatActivity {
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView username;
 
@@ -25,10 +36,40 @@ public class DashboardActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference usersRef;
 
+    private SensorManager sensorManager;
+    private Sensor stepCounterSensor;
+    private Sensor accelerometerSensor;
+
+    private TextView stepsTextView;
+    private TextView distanceTextView;
+    private TextView energyTextView;
+
+    private ProgressBar stepProgressBar;
+    private ProgressBar distanceProgressBar;
+    private ProgressBar energyProgressBar;
+    private int stepsCount = 0;
+    private long lastStepTimestamp = 0;
+
+    private double MagnitudePrevious = 0;
+    // Constant for an average step length in meters
+    private static final float AVERAGE_STEP_LENGTH = 0.76f;
+
+    // Constant for MET (Metabolic Equivalent of Task) value
+    private static final float MET_VALUE = 3.5f;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        stepsTextView = findViewById(R.id.stepsTextView);
+        distanceTextView = findViewById(R.id.distanceTextView);
+        energyTextView = findViewById(R.id.energyTextView);
+
+        stepProgressBar = findViewById(R.id.stepProgressBar);
+        distanceProgressBar = findViewById(R.id.distanceProgressBar);
+        energyProgressBar = findViewById(R.id.energyProgressBar);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -76,5 +117,98 @@ public class DashboardActivity extends AppCompatActivity {
             });
         }
 
+        //Initialize sensors
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+
+        if (stepCounterSensor != null) {
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (accelerometerSensor != null) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            stepsCount = (int) event.values[0];
+            stepsTextView.setText("Steps: " + stepsCount);
+            updateDistanceAndEnergy();
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            handleAccelerometerData(event.values);
+        }
+    }
+
+    private void handleAccelerometerData(float[] values) {
+        // Handle accelerometer data as needed
+        // Example: Detect steps based on acceleration changes
+//        float acceleration = Math.abs(values[0] + values[1] + values[2]);
+//        if (acceleration > 10 && System.currentTimeMillis() - lastStepTimestamp > 500) {
+//            // Assuming a step is detected when acceleration exceeds a threshold
+//            lastStepTimestamp = System.currentTimeMillis();
+//            stepsCount++;
+//            stepsTextView.setText("Steps: " + stepsCount);
+//            updateDistanceAndEnergy();
+//        }
+
+        float x_acceleration = values[0];
+        float y_acceleration = values[1];
+        float z_acceleration = values[2];
+
+        double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+        double MagnitudeDelta = Magnitude - MagnitudePrevious;
+        MagnitudePrevious = Magnitude;
+
+        if (MagnitudeDelta > 6){
+            stepsCount++;
+            stepsTextView.setText("Steps: " + stepsCount);
+            updateDistanceAndEnergy();
+        }
+    }
+
+    private void updateDistanceAndEnergy() {
+        // Calculate distance based on step count and average step length
+        float distance = stepsCount * AVERAGE_STEP_LENGTH;
+        distanceTextView.setText("Distance: " + distance + " meters");
+
+        // Calculate energy expenditure based on MET value
+        float energy = stepsCount * MET_VALUE;
+        energyTextView.setText("Energy: " + energy + " kcal");
+
+        // Define scaling factors for mapping values to progress bar range
+        float distanceScale = 0.2f;  // Example: 1 meter corresponds to 10 progress units
+        float energyScale = 0.1f;    // Example: 1 kcal corresponds to 20 progress units
+        float stepscale = 0.5f;
+
+        // Map calculated values to progress bar ranges
+        int distanceProgress = (int) (distance * distanceScale);
+        int energyProgress = (int) (energy * energyScale);
+        int stepProgress = (int) (stepsCount * stepscale);
+
+        // Update progress bars
+        stepProgressBar.setProgress(stepProgress);
+        distanceProgressBar.setProgress(distanceProgress);
+        energyProgressBar.setProgress(energyProgress);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Handle accuracy changes if needed
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this);
+            stepsCount = 0;
+        }
     }
 }
+
